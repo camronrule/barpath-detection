e0bbabbace9d998d375498e1512ba724e3592167
Camron Rule <camronrule@gmail.com>
2024-12-11 18:37:35 -0500

    data

---
+++ code/data/CV.v15i.yolov11/README.dataset.txt
@@ -0,0 +1,6 @@
+# CV > 2022-05-19 9:34am
+https://universe.roboflow.com/mysteriousbaboon/cv-nr4zq
+
+Provided by a Roboflow user
+License: Public Domain
+
---
+++ code/main.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+# Build a new model
+model = YOLO("yolo11n.yaml")
+
+# Train the model
+train_results = model.train(
+    data="data/CV.v15i.yolov11/data.yaml",  # path to dataset YAML
+    epochs=1,  # number of training epochs
+    imgsz=640,  # training image size
+    device="cpu",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
+)
+
