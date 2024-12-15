8197414b02c6ac65cc220396e3b75281d5fa21da
Camron Rule <camronrule@gmail.com>
2024-12-14 21:02:36 -0500

    Documentation

--- code/main.py
+++ code/main.py
@@ -27,7 +27,6 @@ thickness = sv.calculate_optimal_line_thickness(
     resolution_wh=video_info.resolution_wh)
 text_scale = sv.calculate_optimal_text_scale(resolution_wh=video_info.resolution_wh)
 
-
 tracker = sv.ByteTrack(frame_rate=video_info.fps)
 smoother = sv.DetectionsSmoother()
 
@@ -40,10 +39,6 @@ plate_size_meters = 0.45 # plate diameter in meters
 
 ema_speeds = defaultdict(lambda: None) # use exponential floating avg to smooth speed values
 alpha = 0.5   # smoothing factor (0 < alpha <= 1)
-
-max_speed = max_smoothed_speed = 0
-
-tracker_speeds = {} # keep speeds per each tracker_id, to plot later
 
 # dont post every single speed, to make speed readable
 update_interval = 6 # update speed after this many frames
@@ -64,48 +59,50 @@ acceleration = []
 
 with sv.VideoSink(video_path_out, video_info=video_info) as sink:
     for frame_idx, frame in enumerate(frame_generator): # frame_idx = index of frame
+
         result = model(frame)[0]
         detections = sv.Detections.from_ultralytics(result)
         detections = tracker.update_with_detections(detections)
         detections = smoother.update_with_detections(detections)
 
         # filter detections
-        detections = detections[detections.confidence > 0.5]
-        detections = detections[detections.class_id == 1] # filter to only barbell
-        labels = []
+        detections = detections[detections.confidence > 0.5] # only confident detection
+        detections = detections[detections.class_id == 1] # only barbell
+        labels = [] # labels that will be written to this frame
 
-        if len(detections) > 0: # if barbell is detected
+        if len(detections) > 0: # if a barbell is detected
 
             # get plate size for scaling
             coords = detections.xyxy[0] # [x1, y1, x2, y2]
-            plate_size_pixels = (abs(coords[3]-coords[1]) + abs(coords[2] - coords[1]))/2
+            plate_size_pixels = (abs(coords[3]-coords[1]) + abs(coords[2] - coords[1]))/2 # should be a square, just average height and width as a safeguard
 
             scaling_factor = plate_size_meters / plate_size_pixels
 
-            id = detections.tracker_id[0]
+            id = detections.tracker_id[0] # id of this detection (barbell)
 
             plate_center = detections.get_anchors_coordinates(sv.Position.CENTER)[0] # [x, y]
-            coordinates[id].append(plate_center) # get x and y for displacement
+            coordinates[id].append(plate_center) # keep x,y of the barbell to calculate displacement
 
             if len(coordinates[id]) == 2: # if there is a previous frame
-                #calculate euclidian distance since last frame
+
+                #calculate euclidian distance since last the last frame with previous coordinates
                 (x1, y1) = coordinates[id][0]
                 (x2, y2) = coordinates[id][1]
 
+                # change in x, y (in pixels)
                 delta_x = (x2-x1)
                 delta_y = (y2-y1)
-                pixel_displacement = (delta_x ** 2 + delta_y ** 2) ** 0.5
+                pixel_displacement = (delta_x ** 2 + delta_y ** 2) ** 0.5 # total displacement of x, y combined
 
-                distance_meters = pixel_displacement * scaling_factor
+                # change in x, y (in meters)
                 delta_x_meters = delta_x * scaling_factor
                 delta_y_meters = delta_y * scaling_factor
+                distance_meters = pixel_displacement * scaling_factor
 
-                time_seconds = 1 / video_info.fps
-                speed_mps = distance_meters / time_seconds
+                time_seconds = 1 / video_info.fps # time that has passed since last frame, in seconds
+                speed_mps = distance_meters / time_seconds # meters per second
 
-                if speed_mps > max_speed: max_speed = speed_mps
-
-                # ema smoothing
+                # apply EMA smoothing to speeds (which also applies for velocity, acceleration)
                 previous_ema = ema_speeds[id]
                 if previous_ema is None:
                     ema_speeds[id] = speed_mps
@@ -113,36 +110,36 @@ with sv.VideoSink(video_path_out, video_info=video_info) as sink:
                     ema_speeds[id] = \
                         alpha * speed_mps + (1 - alpha) * previous_ema
                 smoothed_speed = ema_speeds[id]
-                if smoothed_speed > max_smoothed_speed: max_smoothed_speed = smoothed_speed
-                
+
+                # if we have reached the throttle point (update_interval),
+                # then it is time to update the speed displayed on screen
                 frame_counter[id] += 1
                 if frame_counter[id] >= update_interval:
                     #display this speed
                     display_speed[id] = smoothed_speed
-                    frame_counter[id] = 0
-
-                 # Add speed to the tracker-specific list
-                if id not in tracker_speeds:
-                    tracker_speeds[id] = []
-                tracker_speeds[id].append((frame_idx / video_info.fps, smoothed_speed))
+                    frame_counter[id] = 0 # reset throttle count
 
                 # calculate velocity from displacement
+                # velocity = change in distance / change in time
+                # meters per second
                 delta_t = 1 / video_info.fps
                 v_x = delta_x_meters / delta_t
                 v_y = delta_y_meters / delta_t
 
-                #calculate acceleration from velocity
-                if frame_idx > 1: # need to be on the third frame to calc acceleration
+                # calculate acceleration from velocity
+                # acceleration = change in velocity / change in time
+                # meters per second squared
+                if frame_idx > 1: # need to be on the third frame to calc acceleration (velocity not available until second frame)
                     a_x = (v_x - velocity_x[-1]) / delta_t
                     a_y = (v_y - velocity_y[-1]) / delta_t
                     a_total = (a_x ** 2 + a_y ** 2) ** 0.5
                     acceleration.append(a_total)
                 else:
-                    acceleration.append(None)
+                    acceleration.append(None) # to ensure that all data is same length
 
                 # add to output data
                 frame_index.append(frame_idx)
-                x_norm.append(plate_center[0] / video_info.width) # x / width = x_norm
+                x_norm.append(plate_center[0] / video_info.width) # x_norm = x / width
                 y_norm.append(plate_center[1] / video_info.height)
                 delta_x_out.append(delta_x_meters)
                 delta_y_out.append(delta_y_meters)
@@ -150,11 +147,12 @@ with sv.VideoSink(video_path_out, video_info=video_info) as sink:
                 velocity_y.append(v_y)
                 speed.append(speed_mps)
 
-                labels.append(f"{display_speed[id]:.3f} m/s")
+                labels.append(f"{display_speed[id]:.3f} m/s") # show speed
 
             else:
                 labels.append("") # edge case -> first label, not able to calculate speed
 
+                # append empty data
                 frame_index.append(frame_idx)
                 x_norm.append(None) # x / width = x_norm
                 y_norm.append(None)
@@ -170,9 +168,6 @@ with sv.VideoSink(video_path_out, video_info=video_info) as sink:
         annotated_frame = label_annotator.annotate(annotated_frame, detections, labels=labels)
 
         sink.write_frame(annotated_frame)
-
-print(f"Max speed found: {max_speed:.3f} m/s ({max_smoothed_speed:.3f} m/s smoothed)")
-print(len(acceleration))
 
 df = pd.DataFrame({
     "Frame": frame_index,
