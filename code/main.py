85737eef6b0dfd57bee0e18af866fa67a079b41e
Camron Rule <camronrule@gmail.com>
2024-12-14 20:42:47 -0500

    Log velocity, acceleration to csv

--- code/main.py
+++ code/main.py
@@ -1,7 +1,9 @@
 from collections import defaultdict, deque
+import pandas as pd
 import supervision as sv
 from ultralytics import YOLO
 from sys import maxsize
+
 
 '''
 TODO:
@@ -11,10 +13,10 @@ TODO:
 - different color line for each rep
 - 
 '''
-
 model_path = "../runs/detect/train/weights/best.pt"
-video_path = "data/videos/IMG_6860.MOV"
+video_path = "data/videos/IMG_6723.MOV"
 video_path_out = '{}_out.mp4'.format(video_path)
+output_csv_path = '{}_out.csv'.format(video_path)
 
 model = YOLO("../runs/detect/train/weights/best.pt")
 
@@ -48,6 +50,18 @@ update_interval = 6 # update speed after this many frames
 frame_counter = defaultdict(lambda: 0) # holds the frame buffer count for each tracker_id
 display_speed = defaultdict(lambda: 0) # holds the current speed shown on screen for each tracker_id
 
+# for outputting data to csv
+frame_index = []
+x_norm = []
+y_norm = []
+delta_x_out = []
+delta_y_out = []
+speed = []
+velocity = []
+velocity_x = []
+velocity_y = []
+acceleration = [] 
+
 with sv.VideoSink(video_path_out, video_info=video_info) as sink:
     for frame_idx, frame in enumerate(frame_generator): # frame_idx = index of frame
         result = model(frame)[0]
@@ -60,27 +74,32 @@ with sv.VideoSink(video_path_out, video_info=video_info) as sink:
         detections = detections[detections.class_id == 1] # filter to only barbell
         labels = []
 
-        if len(detections) > 0:
+        if len(detections) > 0: # if barbell is detected
 
             # get plate size for scaling
-            coords = detections.xyxy[0]
+            coords = detections.xyxy[0] # [x1, y1, x2, y2]
             plate_size_pixels = (abs(coords[3]-coords[1]) + abs(coords[2] - coords[1]))/2
 
             scaling_factor = plate_size_meters / plate_size_pixels
 
             id = detections.tracker_id[0]
 
-            plate_center = detections.get_anchors_coordinates(sv.Position.CENTER)
-            coordinates[id].append(plate_center[0]) # get the y coord
+            plate_center = detections.get_anchors_coordinates(sv.Position.CENTER)[0] # [x, y]
+            coordinates[id].append(plate_center) # get x and y for displacement
 
-            if len(coordinates[id]) == 2:
+            if len(coordinates[id]) == 2: # if there is a previous frame
                 #calculate euclidian distance since last frame
                 (x1, y1) = coordinates[id][0]
                 (x2, y2) = coordinates[id][1]
 
-                pixel_displacement = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+                delta_x = (x2-x1)
+                delta_y = (y2-y1)
+                pixel_displacement = (delta_x ** 2 + delta_y ** 2) ** 0.5
 
                 distance_meters = pixel_displacement * scaling_factor
+                delta_x_meters = delta_x * scaling_factor
+                delta_y_meters = delta_y * scaling_factor
+
                 time_seconds = 1 / video_info.fps
                 speed_mps = distance_meters / time_seconds
 
@@ -107,10 +126,44 @@ with sv.VideoSink(video_path_out, video_info=video_info) as sink:
                     tracker_speeds[id] = []
                 tracker_speeds[id].append((frame_idx / video_info.fps, smoothed_speed))
 
+                # calculate velocity from displacement
+                delta_t = 1 / video_info.fps
+                v_x = delta_x_meters / delta_t
+                v_y = delta_y_meters / delta_t
+
+                #calculate acceleration from velocity
+                if frame_idx > 1: # need to be on the third frame to calc acceleration
+                    a_x = (v_x - velocity_x[-1]) / delta_t
+                    a_y = (v_y - velocity_y[-1]) / delta_t
+                    a_total = (a_x ** 2 + a_y ** 2) ** 0.5
+                    acceleration.append(a_total)
+                else:
+                    acceleration.append(None)
+
+                # add to output data
+                frame_index.append(frame_idx)
+                x_norm.append(plate_center[0] / video_info.width) # x / width = x_norm
+                y_norm.append(plate_center[1] / video_info.height)
+                delta_x_out.append(delta_x_meters)
+                delta_y_out.append(delta_y_meters)
+                velocity_x.append(v_x)
+                velocity_y.append(v_y)
+                speed.append(speed_mps)
+
                 labels.append(f"{display_speed[id]:.3f} m/s")
 
             else:
                 labels.append("") # edge case -> first label, not able to calculate speed
+
+                frame_index.append(frame_idx)
+                x_norm.append(None) # x / width = x_norm
+                y_norm.append(None)
+                delta_x_out.append(None)
+                delta_y_out.append(None)
+                velocity_x.append(None)
+                velocity_y.append(None)
+                speed.append(None)
+                acceleration.append(None)
 
         annotated_frame = box_annotator.annotate(frame.copy(), detections)
         annotated_frame = trace_annotator.annotate(annotated_frame, detections)
@@ -119,18 +172,18 @@ with sv.VideoSink(video_path_out, video_info=video_info) as sink:
         sink.write_frame(annotated_frame)
 
 print(f"Max speed found: {max_speed:.3f} m/s ({max_smoothed_speed:.3f} m/s smoothed)")
+print(len(acceleration))
 
-import matplotlib.pyplot as plt
+df = pd.DataFrame({
+    "Frame": frame_index,
+    "X_normalized": x_norm,
+    "Y_normalized": y_norm,
+    "Delta_X": delta_x_out,
+    "Delta Y": delta_y_out,
+    "Speed": speed,
+    "Velocity_X": velocity_x,
+    "Velocity_Y": velocity_y,
+    "Acceleration": acceleration
+})
 
-# Plot speeds for each tracker
-plt.figure(figsize=(10, 6))
-for tracker_id, speeds in tracker_speeds.items():
-    times, values = zip(*speeds)  # Separate time and speed values
-    plt.plot(times, values, label=f"Tracker {tracker_id}")
-
-plt.title("Speed Over Time")
-plt.xlabel("Time (seconds)")
-plt.ylabel("Speed (m/s)")
-plt.legend()
-plt.grid()
-plt.show()
+df.to_csv(output_csv_path, index=False)
